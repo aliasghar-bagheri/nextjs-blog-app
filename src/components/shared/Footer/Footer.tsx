f86ab30a693c0e3fab700b1c5ba340f646635b6d
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-16 22:15:47 +0330

    feat: add footer component

---
+++ src/components/shared/Footer/Footer.tsx
@@ -0,0 +1,66 @@
+import Button from '@/components/ui/Button';
+import Input from '@/components/ui/Input';
+import { footerLinks, socialLinks } from '@/constants/mainLinks';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <footer className="bg-secondary-100">
+      <div className="container pt-10 pb-20 flex items-start flex-wrap gap-8 md:gap-16">
+        {footerLinks.map((item) => (
+          <div
+            key={item.title}
+            className="flex-1 text-nowrap group"
+          >
+            <h5 className="group-first-of-type:text-primary-400 text-secondary-900 font-bold text-lg">
+              {item.title}
+            </h5>
+            <ul className="space-y-4 mt-3">
+              {item.links.map((link) => (
+                <li key={link.name}>
+                  <Link href={link.href}>{link.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        <div className="flex-1 text-nowrap">
+          <h5 className="text-secondary-900 font-bold text-lg">
+            برای اطلاع از آخرین اخبار عضو شوید{' '}
+          </h5>
+          <div className="space-y-10">
+            <div className="flex items-center gap-1 mt-3">
+              <Input
+                type="text"
+                placeholder="ایمیل خود را وارد کنید"
+                className="w-full sm:w-60"
+              />
+              <Button variant="primary">عضویت</Button>
+            </div>
+            <div className="flex items-center justify-start gap-6">
+              {socialLinks.map((item) => (
+                <Link
+                  key={item.label}
+                  href={item.href}
+                >
+                  <Image
+                    src={item.icon}
+                    width={30}
+                    height={30}
+                    alt={item.label}
+                  />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full text-center p-3 bg-primary">
+        <p className="text-white font-semibold text-base">
+          © کلیه حقوق مادی و معنوی دوره ها متعلق به این دیجی نیوز می باشد.
+        </p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/constants/mainLinks.tsx
@@ -0,0 +1,76 @@
+export const footerLinks = [
+  {
+    title: 'حامیان ما',
+    links: [
+      {
+        name: 'پارس پک | میزبانی و پشتیبانی',
+        href: '/#',
+      },
+      {
+        name: 'وب رخ | حس خوب پیکسل ها',
+        href: '/#',
+      },
+    ],
+  },
+  {
+    title: 'پرطرفدارترین صفحات',
+    links: [
+      {
+        name: 'تکنولوژی',
+        href: '/#',
+      },
+      {
+        name: 'بررسی ویدیویی',
+        href: '/#',
+      },
+      {
+        name: 'موبایل',
+        href: '/#',
+      },
+      {
+        name: 'آنباکسینگ',
+        href: '/#',
+      },
+    ],
+  },
+  {
+    title: 'دسترسی سریع',
+    links: [
+      {
+        name: 'تماس با ما',
+        href: '/#',
+      },
+      {
+        name: 'درباره ما',
+        href: '/#',
+      },
+      {
+        name: 'استخدام نویسنده',
+        href: '/#',
+      },
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'youtube',
+    href: '/#',
+    icon: '/assets/icons/youtube.svg',
+  },
+  {
+    label: 'instagram',
+    href: '/#',
+    icon: '/assets/icons/instagram.svg',
+  },
+  {
+    label: 'telegram',
+    href: '/#',
+    icon: '/assets/icons/telegram.svg',
+  },
+  {
+    label: 'whatsapp',
+    href: '/#',
+    icon: '/assets/icons/whatsapp.svg',
+  },
+];
