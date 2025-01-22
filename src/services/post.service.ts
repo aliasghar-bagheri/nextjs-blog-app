0bed2368e6beef74bc06e5d91af0f170d09d833b
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-22 17:58:25 +0330

    feat: add function to get a post using slug

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from 'axios';
 import http from './httpService';
 
+// *********** Get all posts
 export async function getAllPosts(queries?: string, options?: AxiosRequestConfig) {
   try {
     const { data } = await http.get(`/post/list?${queries}`, options);
@@ -12,5 +13,19 @@ export async function getAllPosts(queries?: string, options?: AxiosRequestConfig
     console.log(error);
 
     return { posts: [], totalPages: 0 };
+  }
+}
+
+// *********** Get post by slug
+export async function getPostBySlug(slug: string, options?: AxiosRequestConfig) {
+  try {
+    const { data } = await http.get(`/post/slug/${slug}`, options);
+
+    const { post } = data.data || {};
+
+    return { post };
+  } catch (error) {
+    console.log(error);
+    return { post: {} };
   }
 }
