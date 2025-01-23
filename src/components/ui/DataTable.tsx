54336ac5413938e50230a07c116ba38174bd319c
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-23 15:33:29 +0330

    feat: add data table component to display data in table a format

---
+++ src/components/ui/DataTable.tsx
@@ -0,0 +1,52 @@
+import { ReactNode } from 'react';
+import { Table, TableBody, TableData, TableHead, TableHeader, TableRow } from './Table';
+
+export interface DataTableColumn<T> {
+  key: keyof T;
+  label: ReactNode;
+  render?: (item: T) => ReactNode;
+}
+
+export interface DataTableProps<T> {
+  columns: DataTableColumn<T>[];
+  className?: string;
+  data: T[];
+}
+
+const DataTable = <T,>({ className = '', columns, data }: DataTableProps<T>) => {
+  return (
+    <Table className={className}>
+      <TableHeader>
+        <TableRow>
+          {columns.map((column) => (
+            <TableHead key={column.key as string}>{column.label as ReactNode}</TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data && data.length > 0 ? (
+          data.map((item, index) => (
+            <TableRow key={index}>
+              {columns.map((column) => (
+                <TableData key={column.key as string}>
+                  {column.render ? column.render(item) : (item[column.key] as ReactNode)}
+                </TableData>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableData
+              colSpan={7}
+              className="!text-center"
+            >
+              هیچ نتیجه ای یافت نشد
+            </TableData>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default DataTable;
