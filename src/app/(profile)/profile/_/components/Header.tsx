0afeeed21a0b32ce681ffecc573e9cac3122e2fc
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-23 15:23:14 +0330

    feat: add responsive header component

---
+++ src/app/(profile)/profile/_/components/Header.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import Avatar from '@/components/ui/Avatar';
+import ButtonIcon from '@/components/ui/ButtonIcon';
+import Drawer from '@/components/ui/Drawer';
+import { useAuth } from '@/context/AuthContext';
+import { Bars3BottomRightIcon } from '@heroicons/react/24/outline';
+import { BellIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import { useState } from 'react';
+import Sidebar from './Sidebar';
+
+export default function Header() {
+  const { user, isPending } = useAuth();
+  const [isShowHeader, setIsShowHeader] = useState(false);
+
+  return (
+    <header
+      className={clsx('w-full flex items-center justify-between p-6 transition-all duration-300', {
+        'blur-md': isPending,
+      })}
+    >
+      <ButtonIcon
+        onClick={() => setIsShowHeader(true)}
+        className="lg:hidden"
+        variant="outline"
+      >
+        <Bars3BottomRightIcon />
+      </ButtonIcon>
+      <h4 className="text-xl font-semibold hidden sm:block">خوش آمدی {user?.name} عزیز 👋</h4>
+      <div className="flex items-center gap-x-5">
+        <BellIcon
+          width={25}
+          height={25}
+        />
+        <Avatar
+          src={user?.avatarUrl || '/assets/images/user-placeholder.svg'}
+          alt={user?.name || ''}
+        />
+      </div>
+      <Drawer
+        open={isShowHeader}
+        onClose={() => setIsShowHeader(false)}
+      >
+        <div className="flex flex-col justify-between h-full">
+          <Sidebar />
+        </div>
+      </Drawer>
+    </header>
+  );
+}
