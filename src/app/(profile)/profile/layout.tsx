158ebb115ad2bfe2d9a30dcff466bf6cae0850c4
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-23 15:25:43 +0330

    feat: add main layout of the user panel

---
+++ src/app/(profile)/profile/layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import Header from './_/components/Header';
+import Sidebar from './_/components/Sidebar';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    absolute: 'داشبورد',
+  },
+};
+
+export default function UserProfileLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="grid grid-cols-12 max-h-screen gap-x-3 bg-secondary-0">
+      <aside className="hidden lg:flex lg:col-span-3 2xl:col-span-2 h-screen flex-col justify-between p-6">
+        <Sidebar />
+      </aside>
+
+      <div className="col-span-full lg:col-span-9 2xl:col-span-10 h-screen flex flex-col">
+        <Header />
+        <main className="bg-secondary-100 p-8 flex-1 lg:rounded-tr-3xl overflow-y-auto">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
