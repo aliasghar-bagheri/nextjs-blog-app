77d2e4f01b9a9345f3f69f15c20a77a7ed3b987d
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-25 10:48:31 +0330

    refactor: change the modal style to fill its max-width

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -25,7 +25,7 @@ const Modal = ({ open, onClose, title, children }: ModalProps) => {
           {/* Overlay */}
           <div className="fixed pointer-events-none top-0 left-0 w-full h-screen bg-secondary-300/30 backdrop-blur z-50"></div>
           <div className="w-full absolute top-0 left-0 z-[60] h-full flex items-center justify-center px-4">
-            <div className="max-w-xl p-5 rounded space-y-6 bg-secondary-0">
+            <div className="max-w-xl w-full p-5 rounded space-y-6 bg-secondary-0">
               <div className="flex items-center justify-between gap-3 border-b border-b-secondary-200 pb-4">
                 <h5 className="text-xl font-bold">{title}</h5>
                 <ButtonIcon
