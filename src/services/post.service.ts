e9f6ffb71a99a3dc6b41ed056b81ec637e6c5a52
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-25 07:58:07 +0330

    feat: add function to get post by id

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -30,6 +30,20 @@ export async function getPostBySlug(slug: string, options?: AxiosRequestConfig) 
   }
 }
 
+// *********** Get post by id
+export async function getPostByIdApi(postId: string, options?: AxiosRequestConfig) {
+  try {
+    const { data } = await http.get(`/post/${postId}`, options);
+
+    const { post } = data.data || {};
+
+    return { post };
+  } catch (error) {
+    console.log(error);
+    return { post: {} };
+  }
+}
+
 // *********** Like post
 export async function likePostApi(postId: string) {
   return http.post(`/post/like/${postId}`).then(({ data }) => data.data);
