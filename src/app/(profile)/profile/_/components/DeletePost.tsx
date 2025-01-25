4e211a26864f9a925a5a48f57a1a6baae00b732c
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-25 09:42:48 +0330

    feat: add component to delete a post

---
+++ src/app/(profile)/profile/_/components/DeletePost.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import Button from '@/components/ui/Button';
+import ButtonIcon from '@/components/ui/ButtonIcon';
+import Modal from '@/components/ui/Modal';
+import { IPost } from '@/types';
+import { TrashIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import { useDeletePost } from '../../posts/_/hooks/useDeletePost';
+import SpinnerMini from '@/components/ui/SpinnerMini';
+
+export default function DeletePost({ post }: { post: IPost }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const { mutate: deletePost, isPending: isDeleting } = useDeletePost();
+
+  const handleDeletePost = async () => {
+    deletePost(post._id, {
+      onSuccess() {
+        setShowModal(false);
+      },
+    });
+  };
+
+  return (
+    <>
+      <ButtonIcon
+        onClick={() => setShowModal(true)}
+        variant="danger"
+      >
+        <TrashIcon />
+      </ButtonIcon>
+
+      <Modal
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        title="آیا مطمئن هستید ؟"
+      >
+        <div className="space-y-7">
+          <p>
+            با پذیرفتن این عمل, پست <q>{post.title}</q> به صورت کامل حذف میشود و قابل برگشت نیست.
+          </p>
+          <div className="flex items-center gap-x-3">
+            <Button
+              onClick={() => setShowModal(false)}
+              variant="ghost"
+            >
+              انصراف
+            </Button>
+            <Button
+              disabled={isDeleting}
+              className="flex items-center gap-2"
+              variant="danger"
+              onClick={handleDeletePost}
+            >
+              {isDeleting && <SpinnerMini />}
+              پذیرفتن
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+}
