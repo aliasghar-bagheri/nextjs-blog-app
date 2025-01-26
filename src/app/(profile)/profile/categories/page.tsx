20851f9ba79ebc2ef8aa6fb1fcb27d1ae1a8f3cf
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-26 06:30:25 +0330

    feat: add a page to work and display categories

---
+++ src/app/(profile)/profile/categories/page.tsx
@@ -0,0 +1,37 @@
+import { Skeleton } from '@/components/ui/Skeleton';
+import queryString from 'query-string';
+import { Suspense } from 'react';
+import AddCategory from './_/components/AddCategory';
+import CategoriesTable from './_/components/CategoriesTable';
+
+export default async function CategoriesPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string }>;
+}) {
+  const params = await searchParams;
+  const queries = queryString.stringify(params);
+
+  return (
+    <div className="w-full space-y-10">
+      <div className="flex flex-wrap items-center justify-between gap-5">
+        <h2 className="text-secondary-900 font-medium text-xl text-nowrap">لیست دسته بندی ها</h2>
+        <AddCategory />
+      </div>
+      <div className="xl:container space-y-3">
+        <Suspense
+          fallback={
+            <Skeleton
+              width="100%"
+              height={550}
+              className="bg-secondary-200 rounded"
+            />
+          }
+          key={queries}
+        >
+          <CategoriesTable queries={queries} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
