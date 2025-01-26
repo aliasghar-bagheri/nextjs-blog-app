5a10dac78f3189015ef7443bf926d00abaaef877
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-26 06:19:01 +0330

    feat: add function to create a category

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,6 +1,8 @@
 import { AxiosRequestConfig } from 'axios';
 import http from './httpService';
+import { CategorySchemaType } from '@/lib/validations/category/category.schema';
 
+// *********** Get all posts
 export async function getAllCategory(queries?: string, options?: AxiosRequestConfig) {
   try {
     const { data } = await http.get(`/category/list?${queries}`, options);
@@ -14,3 +16,12 @@ export async function getAllCategory(queries?: string, options?: AxiosRequestCon
     return { categories: [] };
   }
 }
+
+// *********** Create new category
+export async function createNewCategoryApi(
+  categoryData: CategorySchemaType,
+  options?: AxiosRequestConfig
+) {
+  return http.post('/category/add', categoryData, options).then(({ data }) => data.data);
+}
+
