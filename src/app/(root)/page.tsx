1c83442f8911e9cdf3edf883dfb3b9b839eb3752
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-26 14:12:12 +0330

    feat: add popular categories with dummy data

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/shared/Hero/HeroSection';
 import LatestArticlesSection from '@/components/shared/latest-articles/LatestArticlesSection';
+import MostPopularCategories from '@/components/shared/MostPopularCategories/MostPopularCategories';
 
 export default function Home() {
   return (
     <div className="space-y-10">
       <HeroSection />
       <LatestArticlesSection />
+      <MostPopularCategories />
     </div>
   );
 }
