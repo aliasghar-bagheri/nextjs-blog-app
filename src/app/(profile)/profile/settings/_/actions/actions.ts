e691aa5e3d6abf7942eb8c4fc964beda1b969ac5
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-01-26 17:47:03 +0330

    feat: add form with actions to update the user information

--- src/app/(profile)/profile/settings/_/actions/actions.ts
+++ src/app/(profile)/profile/settings/_/actions/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { updateAvatar } from '@/services/auth.service';
+import { updateAvatar, updateProfile } from '@/services/auth.service';
 import { handleError } from '@/utils/handleError';
 import setCookieOnReq from '@/utils/setCookieOnReq';
 import { revalidatePath } from 'next/cache';
@@ -15,6 +15,30 @@ export async function uploadAvatar(
     const options = setCookieOnReq(cookie);
 
     const { message } = await updateAvatar(formData, options);
+
+    revalidatePath('/profile/settings');
+
+    return { message, error: '' };
+  } catch (error) {
+    const errorMsg = handleError(error);
+    return { error: errorMsg, message: '' };
+  }
+}
+
+export async function updateInfo(
+  prevState: { message: string; error: string },
+  formData: FormData
+) {
+  try {
+    const cookie = cookies();
+    const options = setCookieOnReq(cookie);
+
+    const data = {
+      name: formData.get('name')?.toString() || '',
+      email: formData.get('email')?.toString() || '',
+    };
+
+    const { message } = await updateProfile(data, options);
 
     revalidatePath('/profile/settings');
 
---
+++ src/app/(profile)/profile/settings/_/form/UserInfoForm.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { IUser } from '@/types';
+import { useFormState } from 'react-dom';
+import { updateInfo } from '../actions/actions';
+import Label from '@/components/ui/Label';
+import Input from '@/components/ui/Input';
+import SubmitButton from '@/components/ui/SubmitButton';
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
+
+const initialState = {
+  error: '',
+  message: '',
+};
+
+export default function UserInfoForm({ initialData }: { initialData: IUser }) {
+  const [state, formAction] = useFormState(updateInfo, initialState);
+
+  useEffect(() => {
+    if (state.message) {
+      toast.success(state.message);
+    }
+
+    if (state.error) {
+      toast.error(state.error);
+    }
+  }, [state]);
+
+  return (
+    <div className="space-y-7">
+      <form
+        action={formAction}
+        className="space-y-3"
+      >
+        <div className="space-y-3">
+          <Label htmlFor="name">نام</Label>
+          <Input
+            type="text"
+            name="name"
+            id="name"
+            className="block w-full"
+            defaultValue={initialData.name}
+          />
+        </div>
+        <div className="space-y-3">
+          <Label htmlFor="email">ایمیل</Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            className="block w-full"
+            defaultValue={initialData.email}
+          />
+        </div>
+        <SubmitButton variant="primary">بروزرسانی</SubmitButton>
+      </form>
+    </div>
+  );
+}
