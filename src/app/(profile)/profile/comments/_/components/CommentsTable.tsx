d87174897cbef78e4416676b3c7f59dcd1b8a912
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-02-01 16:25:39 +0330

    feat: add pagination to the comments table

--- src/app/(profile)/profile/comments/_/components/CommentsTable.tsx
+++ src/app/(profile)/profile/comments/_/components/CommentsTable.tsx
@@ -3,19 +3,29 @@ import { getAllComments } from '@/services/comment.service';
 import setCookieOnReq from '@/utils/setCookieOnReq';
 import { cookies } from 'next/headers';
 import { allCommentsTableColumns } from './columns';
+import Pagination from '@/components/ui/Pagination';
+import { IComment } from '@/types';
 
-export default async function CommentsTable() {
+export default async function CommentsTable({ queries }: { queries: string }) {
   const cookie = cookies();
   const options = setCookieOnReq(cookie);
 
-  const { comments } = await getAllComments(options);
+  const { comments, totalPages } = await getAllComments(queries, options);
+
+  const allComments = (comments as IComment[]).flatMap((comment) => [...comment.answers, comment]);
 
   return (
     <>
       <DataTable
         columns={allCommentsTableColumns}
-        data={comments}
+        data={allComments}
       />
+      <div className="w-full flex items-center justify-center">
+        <Pagination
+          totalPages={totalPages}
+          limit={8}
+        />
+      </div>
     </>
   );
 }
